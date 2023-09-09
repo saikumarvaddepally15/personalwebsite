87bd23299af92f4831e8efad74bb8545a64c1ea2
Sai Kumar Vaddepally <saikumarvaddepally@Sais-MacBook-Pro-6.local>
2023-09-09 12:47:04 -0400

    removed unused imports

--- src/components/SlideBar.js
+++ src/components/SlideBar.js
@@ -1,8 +1,6 @@
 import "../App.css";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import FacebookIcon from "@mui/icons-material/Facebook";
 import { useState } from "react";
 
 export default function Slidebar() {
